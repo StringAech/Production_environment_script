3db246140377862ea34f3c1820ea310deaedd474
guohao <guohao@yucebio.com>
2023-11-15 05:34:48 +0000

    uqdate bitr function

--- Rscript/gene.set.forma.R
+++ Rscript/gene.set.forma.R
@@ -3,25 +3,46 @@ library(dplyr)
 library(openxlsx)
 library(magrittr)
 library(stringr)
-format_gene_set <- function(data,patter="IFN",type=c("list","data.frame")) {
+format_gene_set <- function(data,patter="IFN",type=c("list","data.frame"),is.bitr = FALSE,org = "hsa") {
   #### data: the MSigDB geneset list or other database geneset list 
   #### patter: the character you want to retrieve
   #### type: the results you want to get
   message(paste0("format the gene set frome ",patter))
   message(paste0("The results type is ",type))
-  IFN_names <- grep(pattern = patter,x = names(c2_sets),value = T)
-  IFN_pathway <- c2_sets[IFN_names]
-  IFN_pathway_data.frame <- lapply(names(IFN_pathway), function(.x){
+  names <- grep(pattern = patter,x = names(data),value = T)
+  pathway <- data[names]
+  pathway.data.frame <- lapply(names(pathway), function(.x){
     data <- data.frame(
       pathway = .x,
-      gene = IFN_pathway[[.x]]
+      gene = pathway[[.x]]
     )
     return(data)
   })
-  names(IFN_pathway_data.frame) <- names(IFN_pathway)
-  if(type == "list") { return(IFN_pathway_data.frame) }
-  if(type == "data.frame") {return(Reduce(rbind,IFN_pathway_data.frame) %>% as.data.frame())}
+  names(pathway.data.frame) <- names(pathway)
+  if(is.bitr){ 
+    suppressPackageStartupMessages(library(clusterProfiler))
+    switch (org,
+            hsa = {
+              suppressPackageStartupMessages(library(org.Hs.eg.db))
+              pathway.data.frame <- suppressMessages(lapply(names(pathway.data.frame), function(.x){
+                eg = bitr(pathway.data.frame[[.x]]$gene, fromType = "ENTREZID", toType = "SYMBOL", 
+                          OrgDb = "org.Hs.eg.db") %>% mutate(pathway = .x) %>% dplyr::select(3,2,1)
+              }))
+              names(pathway.data.frame) <- names(pathway.data.frame)
+            },
+            mm = {
+              suppressPackageStartupMessages(library(org.Mm.eg.db))
+              pathway.data.frame <- suppressMessages(lapply(names(pathway.data.frame), function(.x){
+                eg = bitr(pathway.data.frame[[.x]]$gene, fromType = "ENTREZID", toType = "SYMBOL", 
+                          OrgDb = "org.Mm.eg.db") %>% mutate(pathway = .x) %>% dplyr::select(3,2,1)
+              }))
+              names(pathway.data.frame) <- names(pathway.data.frame)
+            }
+    )
+  }
+  if(type == "list") {return(pathway.data.frame) }
+  if(type == "data.frame") {return(Reduce(rbind,pathway.data.frame) %>% as.data.frame())}
 }
-c2_sets <- readRDS("../MsigDB/Hs.c2.all.v7.1.entrez.rds")
-IFN_pathway <- format_gene_set(c2_sets,patter = "IFN",type = "data.frame")
-CD8_pathway <- format_gene_set(c2_sets,patter = "CD8",type = "data.frame")
+c2_sets <- readRDS("~/script/MsigDB/Hs.c2.all.v7.1.entrez.rds")
+IFN_pathway <- format_gene_set(c2_sets,patter = "IFN",type = "data.frame",is.bitr = T,org = "hsa")
+CD8_pathway <- format_gene_set(c2_sets,patter = "CD8",type = "data.frame",is.bitr = T,org = "has")
